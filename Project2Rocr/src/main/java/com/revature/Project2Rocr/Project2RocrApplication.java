71cca3001fee0bdc7cb5fec5bd8d65a3ac885939
Rhys_Yamasaki <yamasaki.rhys@gmail.com>
2018-09-01 15:27:34 -0400

    Server-side maven project

---
+++ Project2Rocr/src/main/java/com/revature/Project2Rocr/Project2RocrApplication.java
@@ -0,0 +1,12 @@
+package com.revature.Project2Rocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2RocrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2RocrApplication.class, args);
+	}
+}
---
+++ Project2Rocr/src/test/java/com/revature/Project2Rocr/Project2RocrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.revature.Project2Rocr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Project2RocrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
