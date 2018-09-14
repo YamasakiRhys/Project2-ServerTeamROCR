8170c6c2797eb60855221fdf09b1e069ff931eb9
Rhys_Yamasaki <yamasaki.rhys@gmail.com>
2018-09-14 14:54:55 -0400

    added swagger and can update requests

--- src/main/java/com/revature/Project2Rocr/rest/TradeCtrl.java
+++ src/main/java/com/revature/Project2Rocr/rest/TradeCtrl.java
@@ -5,15 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.Project2Rocr.beans.Requests;
 import com.revature.Project2Rocr.beans.Trade;
 import com.revature.Project2Rocr.service.RequestsService;
 import com.revature.Project2Rocr.service.TradeService;
+
+@CrossOrigin
 @RestController
 public class TradeCtrl {
 
@@ -40,7 +42,6 @@ public class TradeCtrl {
 	@PostMapping("/trade")
 	public ResponseEntity<Trade> createTrade(@RequestBody Trade trad) {
 		System.out.println("TradeCtrl - createTrade");
-//		req = requestsService.getRequestBy(req.getRequestId());
 		trad = tradeService.createTrade(trad);
 		return new ResponseEntity<Trade>(trad, HttpStatus.CREATED);
 		}
--- src/main/java/com/revature/Project2Rocr/service/RequestsService.java
+++ src/main/java/com/revature/Project2Rocr/service/RequestsService.java
@@ -1,6 +1,7 @@
 package com.revature.Project2Rocr.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,5 +39,15 @@ public class RequestsService {
 		return req;
 		}
 	}
+	
+	public Optional<Requests> findRequestById(int id) {
+		Optional<Requests> req = requestsRepo.findById(id);
+		return req;
+	}
+//	
+//	public Requests updateRequestStatusPending(int id) {
+//		System.out.println("req service - update status to pending");
+//		this.findRequestById(id).get().setRequestId(2);
+//	}
 	
 }
