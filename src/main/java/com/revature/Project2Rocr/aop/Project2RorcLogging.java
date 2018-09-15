4b43c05c25df61952abedb3506b672e9dc759bf3
Rhys_Yamasaki <yamasaki.rhys@gmail.com>
2018-09-15 00:57:27 -0400

    added code for logging and can log with log4j

--- src/main/java/com/revature/Project2Rocr/aop/Project2RorcLogging.java
+++ src/main/java/com/revature/Project2Rocr/aop/Project2RorcLogging.java
@@ -1,17 +1,16 @@
 package com.revature.Project2Rocr.aop;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 @Aspect
 @Configuration
 public class Project2RorcLogging {
 
-private Logger logger = LoggerFactory.getLogger(this.getClass());
+private Logger logger = Logger.getLogger(this.getClass());
     
     
     @After("execution( * com.revature.Project2Rocr.service.*.*(..))")
@@ -19,7 +18,7 @@ private Logger logger = LoggerFactory.getLogger(this.getClass());
         System.out.println("Current Directory = " + System.getProperty("user.dir"));
         System.out.println("LOGGING");
         logger.info("Using a service");
-        logger.info("Allowed execution for {}", joinPoint);
         logger.error("This is an error!");
+        logger.debug("Debugging...");
     }
 }
